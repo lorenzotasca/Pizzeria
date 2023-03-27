db75ed2751bc67f8eeb8c998f8bbf7411ce1eb73
lorenzotasca <101709418+lorenzotasca@users.noreply.github.com>
2023-03-27 12:52:41 +0200

    Add files via upload

---
+++ Pizzeria/Pizzeria.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Pizzeria extends JFrame
+{
+    public Pizzeria(){
+        init();
+    }
+    
+    public void init(){
+        setLayout(null);
+        setSize(WIDTH, HEIGHT);
+        show();
+    }
+}
